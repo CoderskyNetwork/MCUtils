6a97418f2fd54c55a53edc7ba8f0e763e7b8b999
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-06-09 05:14:11 +0200

    Initial commit - Basic plugin setup

---
+++ src/main/java/es/xdec0de/mcutils/MCUtils.java
@@ -0,0 +1,16 @@
+package es.xdec0de.mcutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MCUtils extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+
+	@Override
+	public void onDisable() {
+		
+	}
+}
