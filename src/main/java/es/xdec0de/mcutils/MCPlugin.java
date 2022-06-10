9bf5f73e6c2c72179b167179487aa85e9569b956
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-06-10 05:17:16 +0200

    MCPlugin#getLatestVersion is no longer async
    - The scanner now also gets closed :)

--- src/main/java/es/xdec0de/mcutils/MCPlugin.java
+++ src/main/java/es/xdec0de/mcutils/MCPlugin.java
@@ -12,6 +12,7 @@ import java.util.function.Consumer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
 
 import es.xdec0de.mcutils.files.MCFile;
 import es.xdec0de.mcutils.server.MCVersion;
@@ -49,14 +50,36 @@ public class MCPlugin extends JavaPlugin {
 		return MCVersion.UNKNOWN;
 	}
 
+	/**
+	 * <b>WARNING:</b> This method does <i>NOT</i> run asynchronously by itself, it's recommended to do so by using:
+	 * {@link BukkitScheduler#runTaskAsynchronously(org.bukkit.plugin.Plugin, Consumer)}
+	 * <p><p>
+	 * Gets the latest version of a plugin on <b>consumer</b> using spigotmc's API.
+	 * Version can take a time to update after a new resource update.
+	 * <p><p>
+	 * Example usage from a class extending {@link MCPlugin}:
+	 * <p>
+	 * <code>Bukkit.getScheduler().runTaskAsynchronously(this, () -> getLatestVersion(42, ver -> { } ));</code>
+	 * 
+	 * @param resourceId the ID of the resource to retrieve, the ID can be found at your resource url:
+	 * <i>{@literal https://www.spigotmc.org/resources/<name>.<ID IS HERE>/}</i>
+	 * @param consumer the consumer that will accept the version string, note that this string can be null if
+	 * any error occurred while retrieving the latest version.
+	 * 
+	 * @since MCUtils 1.0.0
+	 * 
+	 * @see Bukkit#getScheduler()
+	 * @see BukkitScheduler#runTaskAsynchronously(org.bukkit.plugin.Plugin, Consumer)
+	 */
 	public void getLatestVersion(int resourceId, Consumer<String> consumer) {
-		Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
-			try (InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + resourceId).openStream(); Scanner scanner = new Scanner(inputStream)) {
-				if (scanner.hasNext())
-					consumer.accept(scanner.next());
-			} catch (IOException ex) {
-				consumer.accept(null);
-			}
-		});
+		try {
+			InputStream inputStream = new URL("https://api.spigotmc.org/legacy/update.php?resource=" + resourceId).openStream();
+			Scanner scanner = new Scanner(inputStream);
+			if (scanner.hasNext())
+				consumer.accept(scanner.next());
+			scanner.close();
+		} catch (IOException ex) {
+			consumer.accept(null);
+		}
 	}
 }
