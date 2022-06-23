839caca6dda93d729ac830998775ce6c150a4ea1
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-06-23 11:51:53 +0200

    MCVersion: Fix 1.10+ versions

--- src/main/java/es/xdec0de/mcutils/server/MCVersion.java
+++ src/main/java/es/xdec0de/mcutils/server/MCVersion.java
@@ -2,26 +2,26 @@ package es.xdec0de.mcutils.server;
 
 public enum MCVersion {
 
-	V1_0,
-	V1_1,
-	V1_2,
-	V1_3,
-	V1_4,
-	V1_5,
-	V1_6,
-	V1_7,
-	V1_8,
-	V1_9,
-	V1_10,
-	V1_11,
-	V1_12,
-	V1_13,
-	V1_14,
-	V1_15,
-	V1_16,
-	V1_17,
-	V1_18,
 	V1_19,
+	V1_18,
+	V1_17,
+	V1_16,
+	V1_15,
+	V1_14,
+	V1_13,
+	V1_12,
+	V1_11,
+	V1_10,
+	V1_9,
+	V1_8,
+	V1_7,
+	V1_6,
+	V1_5,
+	V1_4,
+	V1_3,
+	V1_2,
+	V1_1,
+	V1_0,
 	UNKNOWN;
 
 	public String getFormatName() {
@@ -29,6 +29,6 @@ public enum MCVersion {
 	}
 
 	public boolean supports(MCVersion version) {
-		return ordinal() >= version.ordinal();
+		return ordinal() <= version.ordinal();
 	}
 }
