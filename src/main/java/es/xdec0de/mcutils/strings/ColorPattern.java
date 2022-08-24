a69966d28cad2b51e74f665a8648296544086c25
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-08-24 06:02:30 +0200

    ColorPattern: Support "simple" mode

--- src/main/java/es/xdec0de/mcutils/strings/ColorPattern.java
+++ src/main/java/es/xdec0de/mcutils/strings/ColorPattern.java
@@ -4,9 +4,6 @@ import javax.annotation.Nullable;
 
 /**
  * Represents a color pattern which can be applied to a String.
- * <p>
- * This is an adaptation to MCUtils from the
- * <a href="https://github.com/Iridium-Development/IridiumColorAPI">IridiumColorAPI</a>
  * 
  * @since MCUtils 1.0.0
  */
@@ -14,7 +11,7 @@ public interface ColorPattern {
 
 	/**
 	 * Applies this pattern to the provided <b>string</b>.
-	 * Output might me the same as the input if this pattern is not present.
+	 * Output might be the same as the input if this pattern is not present.
 	 * If the <b>string</b> is null, null will be returned.
 	 *
 	 * @param string the string to which this pattern should be applied to.
@@ -23,4 +20,24 @@ public interface ColorPattern {
 	 */
 	@Nullable
 	String process(@Nullable String string);
+
+	/**
+	 * Applies this pattern to the provided <b>string</b> with an optional simple mode.
+	 * Output might me the same as the input if this pattern is not present.
+	 * If the <b>string</b> is null, null will be returned.
+	 * <p>
+	 * Simple mode is designed for hexadecimal based color patterns, adding an extra
+	 * step to support a three-character hexadecimal color pattern, for example "#FFF".
+	 * If this color pattern doesn't support simple mode <i>(Which is likely if you are reading
+	 * this instead of it's own documentation)</i>, {@link #process(String)} will be used.
+	 * 
+	 * @param string the string to which this pattern should be applied to.
+	 * @param simple whether to use the simple color pattern or not.
+	 * 
+	 * @return The new string with applied pattern.
+	 */
+	@Nullable
+	default String process(@Nullable String string, boolean simple) {
+		return process(string);
+	}
 }
