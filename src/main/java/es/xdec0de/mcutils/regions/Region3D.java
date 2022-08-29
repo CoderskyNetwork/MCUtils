c012a759ef377e70ba749cfe53b2a73dfef88133
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-08-29 11:52:47 +0200

    Rename: Cuboid -> Region3D

--- src/main/java/es/xdec0de/mcutils/regions/Region3D.java
+++ src/main/java/es/xdec0de/mcutils/regions/Region3D.java
@@ -10,7 +10,7 @@ import org.bukkit.World;
  * A class to represent a 3D region.
  * <p>
  * Code has been taken, modified and documented from
- * <a href=https://www.spigotmc.org/threads/between-two-locations-cuboid.325266/>this</a> post.
+ * <a href=https://www.spigotmc.org/threads/between-two-locations-region.325266/>this</a> post.
  * If you like this implementation, please leave a positive rating on BillyGalbreath's reply.
  * 
  * @since MCUtils v1.0.0
@@ -18,13 +18,13 @@ import org.bukkit.World;
  * @author <a href=https://www.spigotmc.org/members/billygalbreath.29442/>BillyGalbreath</a>
  * @author xDec0de_
  */
-public class Cuboid {
+public class Region3D {
 
 	private final World world;
 	private final int minX, maxX, minY, maxY, minZ, maxZ;
 
 	/**
-	 * Creates a 3D region as a cuboid from two locations.
+	 * Creates a 3D region from two locations.
 	 * 
 	 * @param loc1 the first location.
 	 * @param loc2 the second location.
@@ -35,9 +35,9 @@ public class Cuboid {
 	 * 
 	 * @since MCUtils v1.0.0
 	 * 
-	 * @see #Cuboid(World, int, int, int, int, int, int)
+	 * @see #Region3D(World, int, int, int, int, int, int)
 	 */
-	public Cuboid(@Nonnull Location loc1, @Nonnull Location loc2) {
+	public Region3D(@Nonnull Location loc1, @Nonnull Location loc2) {
 		if (loc1 == null || loc2 == null)
 			throw new NullPointerException("Cuboid locations cannot be null.");
 		if (loc1.getWorld() == null || loc2.getWorld() == null)
@@ -56,9 +56,9 @@ public class Cuboid {
 	}
 
 	/**
-	 * Creates a 3D region as a cuboid from two locations by their coordinates.
+	 * Creates a 3D region from two locations by their coordinates.
 	 * 
-	 * @param world the world this cuboid will be in.
+	 * @param world the world this region will be in.
 	 * @param x1 the x coordinate of the first location.
 	 * @param y1 the y coordinate of the first location.
 	 * @param z1 the z coordinate of the first location.
@@ -70,9 +70,9 @@ public class Cuboid {
 	 * 
 	 * @since MCUtils v1.0.0
 	 * 
-	 * @see #Cuboid(Location, Location)
+	 * @see #Region3D(Location, Location)
 	 */
-	public Cuboid(@Nonnull World world, int x1, int y1, int z1, int x2, int y2, int z2) {
+	public Region3D(@Nonnull World world, int x1, int y1, int z1, int x2, int y2, int z2) {
 		if (world == null)
 			throw new NullPointerException("Cuboid world cannot be null.");
 		this.world = world;
@@ -86,9 +86,9 @@ public class Cuboid {
 	}
 
 	/**
-	 * Gets the {@link World} this cuboid is in.
+	 * Gets the {@link World} this {@link Region3D} is in.
 	 * 
-	 * @return The {@link World} this cuboid is in.
+	 * @return The {@link World} this {@link Region3D} is in.
 	 * 
 	 * @since MCUtils v1.0.0
 	 */
@@ -98,9 +98,9 @@ public class Cuboid {
 	}
 
 	/**
-	 * Gets the minimum x coordinate of this {@link Cuboid}.
+	 * Gets the minimum x coordinate of this {@link Region3D}.
 	 * 
-	 * @return The minimum x coordinate of this {@link Cuboid}.
+	 * @return The minimum x coordinate of this {@link Region3D}.
 	 * 
 	 * @since MCUtils v1.0.0
 	 */
@@ -109,9 +109,9 @@ public class Cuboid {
 	}
 
 	/**
-	 * Gets the minimum y coordinate of this {@link Cuboid}.
+	 * Gets the minimum y coordinate of this {@link Region3D}.
 	 * 
-	 * @return The minimum y coordinate of this {@link Cuboid}.
+	 * @return The minimum y coordinate of this {@link Region3D}.
 	 * 
 	 * @since MCUtils v1.0.0
 	 */
@@ -120,9 +120,9 @@ public class Cuboid {
 	}
 
 	/**
-	 * Gets the minimum z coordinate of this {@link Cuboid}.
+	 * Gets the minimum z coordinate of this {@link Region3D}.
 	 * 
-	 * @return The minimum z coordinate of this {@link Cuboid}.
+	 * @return The minimum z coordinate of this {@link Region3D}.
 	 * 
 	 * @since MCUtils v1.0.0
 	 */
@@ -131,9 +131,9 @@ public class Cuboid {
 	}
 
 	/**
-	 * Gets the maximum x coordinate of this {@link Cuboid}.
+	 * Gets the maximum x coordinate of this {@link Region3D}.
 	 * 
-	 * @return The maximum x coordinate of this {@link Cuboid}.
+	 * @return The maximum x coordinate of this {@link Region3D}.
 	 * 
 	 * @since MCUtils v1.0.0
 	 */
@@ -142,9 +142,9 @@ public class Cuboid {
 	}
 
 	/**
-	 * Gets the maximum y coordinate of this {@link Cuboid}.
+	 * Gets the maximum y coordinate of this {@link Region3D}.
 	 * 
-	 * @return The maximum y coordinate of this {@link Cuboid}.
+	 * @return The maximum y coordinate of this {@link Region3D}.
 	 * 
 	 * @since MCUtils v1.0.0
 	 */
@@ -153,9 +153,9 @@ public class Cuboid {
 	}
 
 	/**
-	 * Gets the maximum z coordinate of this {@link Cuboid}.
+	 * Gets the maximum z coordinate of this {@link Region3D}.
 	 * 
-	 * @return The maximum z coordinate of this {@link Cuboid}.
+	 * @return The maximum z coordinate of this {@link Region3D}.
 	 * 
 	 * @since MCUtils v1.0.0
 	 */
@@ -164,41 +164,41 @@ public class Cuboid {
 	}
 
 	/**
-	 * Checks if <b>cuboid</b> is inside of this {@link Cuboid}.
+	 * Checks if <b>region</b> is inside of this {@link Region3D}.
 	 * 
-	 * @param cuboid the {@link Cuboid} to check.
+	 * @param region the {@link Region3D} to check.
 	 * 
-	 * @return True only if <b>cuboid</b> is <b>totally</b>
-	 * inside of this {@link Cuboid}, if <b>cuboid</b> is null, false will be returned.
+	 * @return True only if <b>region</b> is <b>totally</b>
+	 * inside of this {@link Region3D}, if <b>region</b> is null, false will be returned.
 	 * 
 	 * @since MCUtils v1.0.0
 	 * 
-	 * @see #overlaps(Cuboid)
+	 * @see #overlaps(Region3D)
 	 * @see #contains(Location)
 	 * @see #contains(int, int, int)
 	 */
-	public boolean contains(@Nullable Cuboid cuboid) {
-		if (cuboid == null)
+	public boolean contains(@Nullable Region3D region) {
+		if (region == null)
 			return false;
-		return cuboid.getWorld().equals(world) &&
-				cuboid.getMinX() >= minX && cuboid.getMaxX() <= maxX &&
-				cuboid.getMinY() >= minY && cuboid.getMaxY() <= maxY &&
-				cuboid.getMinZ() >= minZ && cuboid.getMaxZ() <= maxZ;
+		return region.getWorld().equals(world) &&
+				region.getMinX() >= minX && region.getMaxX() <= maxX &&
+				region.getMinY() >= minY && region.getMaxY() <= maxY &&
+				region.getMinZ() >= minZ && region.getMaxZ() <= maxZ;
 	}
 
 	/**
-	 * Checks if this {@link Cuboid} contains the specified <b>location</b>.
+	 * Checks if this {@link Region3D} contains the specified <b>location</b>.
 	 * 
 	 * @param location the {@link Location} to check.
 	 * 
-	 * @return True if this {@link Cuboid} contains the specified <b>location</b>, if
+	 * @return True if this {@link Region3D} contains the specified <b>location</b>, if
 	 * <b>location</b>'s {@link World} isn't equal to {@link #getWorld()}, false will be
 	 * returned even if coordinates match, if you just want to check coordinates then use
 	 * {@link #contains(int, int, int)}, if <b>location</b> is null, false will also be returned.
 	 * 
 	 * @since MCUtils v1.0.0
 	 * 
-	 * @see #contains(Cuboid)
+	 * @see #contains(Region3D)
 	 * @see #contains(int, int, int)
 	 */
 	public boolean contains(@Nullable Location location) {
@@ -208,17 +208,17 @@ public class Cuboid {
 	}
 
 	/**
-	 * Checks if this {@link Cuboid} contains the specified coordinates.
+	 * Checks if this {@link Region3D} contains the specified coordinates.
 	 * 
 	 * @param x the x coordinate to check.
 	 * @param y the y coordinate to check.
 	 * @param z the z coordinate to check.
 	 * 
-	 * @return True if this {@link Cuboid} contains the specified coordinates, false otherwise.
+	 * @return True if this {@link Region3D} contains the specified coordinates, false otherwise.
 	 * 
 	 * @since MCUtils v1.0.0
 	 * 
-	 * @see #contains(Cuboid)
+	 * @see #contains(Region3D)
 	 * @see #contains(Location)
 	 */
 	public boolean contains(int x, int y, int z) {
@@ -228,32 +228,32 @@ public class Cuboid {
 	}
 
 	/**
-	 * Checks if this {@link Cuboid} overlaps with <b>cuboid</b>
+	 * Checks if this {@link Region3D} overlaps with <b>region</b>
 	 * 
-	 * @param cuboid the {@link Cuboid} to check.
+	 * @param region the {@link Region3D} to check.
 	 * 
-	 * @return True if this {@link Cuboid} overlaps with <b>cuboid</b>, false
-	 * otherwise or if <b>cuboid</b> is null.
+	 * @return True if this {@link Region3D} overlaps with <b>region</b>, false
+	 * otherwise or if <b>region</b> is null.
 	 * 
 	 * @since MCUtils v1.0.0
 	 * 
-	 * @see #contains(Cuboid)
+	 * @see #contains(Region3D)
 	 */
-	public boolean overlaps(@Nullable Cuboid cuboid) {
-		if (cuboid == null)
+	public boolean overlaps(@Nullable Region3D region) {
+		if (region == null)
 			return false;
-		return cuboid.getWorld().equals(world) &&
-				!(cuboid.getMinX() > maxX || cuboid.getMinY() > maxY || cuboid.getMinZ() > maxZ ||
-						minZ > cuboid.getMaxX() || minY > cuboid.getMaxY() || minZ > cuboid.getMaxZ());
+		return region.getWorld().equals(world) &&
+				!(region.getMinX() > maxX || region.getMinY() > maxY || region.getMinZ() > maxZ ||
+						minZ > region.getMaxX() || minY > region.getMaxY() || minZ > region.getMaxZ());
 	}
 
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null || !(obj instanceof Cuboid))
+		if (obj == null || !(obj instanceof Region3D))
 			return false;
-		final Cuboid other = (Cuboid) obj;
+		final Region3D other = (Region3D) obj;
 		return world.equals(other.world)
 				&& minX == other.minX
 				&& minY == other.minY
@@ -264,15 +264,15 @@ public class Cuboid {
 	}
 
 	/**
-	 * Returns a string representation of this {@link Cuboid} following this format:
+	 * Returns a string representation of this {@link Region3D} following this format:
 	 * <p>
-	 * "Cuboid[world:world_name, minX:X, minY:Y, minZ:Z, maxX:X, maxY:Y, maxZ:Z]";
+	 * "Region3D[world:world_name, minX:X, minY:Y, minZ:Z, maxX:X, maxY:Y, maxZ:Z]";
 	 * 
-	 * @return A string representation of this {@link Cuboid}.
+	 * @return A string representation of this {@link Region3D}.
 	 */
 	@Override
 	public String toString() {
-		return "Cuboid[world:" + world.getName() +
+		return "Region3D[world:" + world.getName() +
 				", minX:" + minX +
 				", minY:" + minY +
 				", minZ:" + minZ +
