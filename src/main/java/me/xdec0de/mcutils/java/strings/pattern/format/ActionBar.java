763c1dcef3f91975275195c8ba3ca16b7a78dbe7
xDec0de_ <xdec0deyt@gmail.com>
2023-08-24 21:52:42 +0200

    Simplified ActionBar FormatPattern (Quite a lot)
    This change will likely be applied to other target patterns if possible to avoid code duplication and to ensure that every pattern behaves the same.

--- src/main/java/me/xdec0de/mcutils/java/strings/pattern/format/ActionBar.java
+++ src/main/java/me/xdec0de/mcutils/java/strings/pattern/format/ActionBar.java
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Player.Spigot;
 
+import me.xdec0de.mcutils.java.strings.MCStrings;
 import me.xdec0de.mcutils.java.strings.pattern.FormatPattern;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -15,18 +16,10 @@ public class ActionBar implements FormatPattern {
 
 	@Nullable
 	public String process(@Nonnull CommandSender target, @Nullable String message) {
-		final StringBuffer res = new StringBuffer(message);
 		final Spigot receiver = target instanceof Player ? ((Player)target).spigot() : null;
-		int start = message.indexOf("<ab:", 0);
-		while (start != -1) {
-			final int end = message.indexOf("/ab>", start);
-			if (end != -1) {
-				if (receiver != null)
-					receiver.sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message.substring(start + 4, end)));
-				res.replace(start, end + 4, ""); // We still want to remove the pattern for others.
-			}
-			start = message.indexOf("<ab:", start + 1);
-		}
-		return res.toString();
+		return MCStrings.matchAndAccept(message, "<ab:", "/ab>", ab -> {
+			if (receiver != null)
+				receiver.sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(ab));
+		});
 	}
 }
