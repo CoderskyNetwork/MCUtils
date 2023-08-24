a25045cd84358d36727afbfde8a57e2b011b01ba
xDec0de_ <xdec0deyt@gmail.com>
2023-08-24 12:55:41 +0200

    ActionBar FormatPattern: Stop using regex

--- src/main/java/me/xdec0de/mcutils/java/strings/pattern/format/ActionBar.java
+++ src/main/java/me/xdec0de/mcutils/java/strings/pattern/format/ActionBar.java
@@ -1,13 +1,11 @@
 package me.xdec0de.mcutils.java.strings.pattern.format;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Player.Spigot;
 
 import me.xdec0de.mcutils.java.strings.pattern.FormatPattern;
 import net.md_5.bungee.api.ChatMessageType;
@@ -15,18 +13,20 @@ import net.md_5.bungee.api.chat.TextComponent;
 
 public class ActionBar implements FormatPattern {
 
-	private final Pattern pattern = Pattern.compile("<ab:(.*?)[/]ab>");
-
 	@Nullable
-	public String process(@Nonnull CommandSender target, @Nullable String string) {
-		String postProcess = string;
-		Matcher matcher = pattern.matcher(string);
-		if (matcher.find()) {
-			final String toAction = matcher.group(1);
-			if (target instanceof Player)
-				((Player)target).spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(toAction));
-			postProcess = matcher.replaceFirst("");
+	public String process(@Nonnull CommandSender target, @Nullable String message) {
+		final StringBuffer res = new StringBuffer(message);
+		final Spigot receiver = target instanceof Player ? ((Player)target).spigot() : null;
+		int start = message.indexOf("<ab:", 0);
+		while (start != -1) {
+			final int end = message.indexOf("/ab>", start);
+			if (end != -1) {
+				if (receiver != null)
+					receiver.sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message.substring(start + 4, end)));
+				res.replace(start, end + 4, ""); // We still want to remove the pattern for others.
+			}
+			start = message.indexOf("<ab:", start + 1);
 		}
-		return postProcess;
+		return res.toString();
 	}
 }
