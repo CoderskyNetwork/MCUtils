9cb3b2aa61dd930a4128868d6015d3fc99c5f500
xDec0de_ <xdec0deyt@gmail.com>
2023-08-25 04:11:05 +0200

    TargetPattern: Stop using regex

--- src/main/java/me/xdec0de/mcutils/java/strings/pattern/format/TargetPattern.java
+++ src/main/java/me/xdec0de/mcutils/java/strings/pattern/format/TargetPattern.java
@@ -1,8 +1,5 @@
 package me.xdec0de.mcutils.java.strings.pattern.format;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
@@ -14,24 +11,22 @@ import me.xdec0de.mcutils.java.strings.pattern.FormatPattern;
 
 public class TargetPattern implements FormatPattern {
 
-	private final Pattern player = Pattern.compile("<p:(.*?)[/]p>");
-	private final Pattern console = Pattern.compile("<c:(.*?)[/]c>");
-
 	@Nullable
-	public String process(@Nonnull CommandSender target, @Nullable String string) {
-		String postProcess = string;
-		final boolean isPlayer = target instanceof Player;
-		Matcher matcher = isPlayer ? player.matcher(postProcess) : console.matcher(postProcess);
-		while (matcher.find()) {
-			if (isPlayer)
-				((Player)target).spigot().sendMessage(MCStrings.applyEventPatterns(matcher.group(1)));
-			else
-				target.sendMessage(matcher.group(1));
-			matcher.reset((postProcess = matcher.replaceFirst("")));
-		}
-		matcher.usePattern(isPlayer ? console : player);
-		while (matcher.find())
-			matcher.reset((postProcess = matcher.replaceFirst("")));
-		return postProcess;
+	public String process(@Nonnull CommandSender target, @Nullable String message) {
+		final Player asPlayer = target instanceof Player ? (Player)target : null;
+		// Process for players
+		String processed = MCStrings.matchAndAccept(message, "<p:", "/p>", msg -> {
+			if (asPlayer != null)
+				asPlayer.sendMessage(msg);
+		});
+		// Process for the console
+		processed = MCStrings.matchAndAccept(processed, "<c:", "/c>", msg -> {
+			if (asPlayer == null)
+				target.sendMessage(msg);
+		});
+		// Send any message left for both players and the console.
+		if (MCStrings.hasContent(processed))
+			target.sendMessage(processed);
+		return processed;
 	}
 }
