86f58d43ba9fe1b1eb0982ba6bd3d6a71607ebf1
xDec0de_ <xdec0deyt@gmail.com>
2023-09-08 22:28:07 +0200

    Added CancellableMCEvent
    Extending this class makes it impossible to extend, for example, PlayerEvent, but for simple events that only need to be cancellable, this may be a good option.

---
+++ src/main/java/me/xdec0de/mcutils/events/CancellableMCEvent.java
@@ -0,0 +1,28 @@
+package me.xdec0de.mcutils.events;
+
+import org.bukkit.event.Cancellable;
+
+/**
+ * A simple type of {@link MCEvent} that
+ * implements the {@link Cancellable} interface
+ * so you don't have to manually do it, see
+ * {@link MCEvent} for more details.
+ * 
+ * @author xDec0de_
+ *
+ * @since MCUtils 1.0.0
+ */
+public abstract class CancellableMCEvent extends MCEvent implements Cancellable {
+
+	private boolean cancelled = false;
+
+	@Override
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancel) {
+		this.cancelled = cancel;
+	}
+}
