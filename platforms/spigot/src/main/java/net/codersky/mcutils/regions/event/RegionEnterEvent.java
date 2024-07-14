a33d126ca61d6df6c114ac47478d6ba91891dbcc
xDec0de_ <xdec0deyt@gmail.com>
2024-07-14 15:56:48 +0200

    Added RegionHandler & region events

---
+++ platforms/spigot/src/main/java/net/codersky/mcutils/regions/event/RegionEnterEvent.java
@@ -0,0 +1,36 @@
+package net.codersky.mcutils.regions.event;
+
+import net.codersky.mcutils.events.player.CancellableMCPlayerEvent;
+import net.codersky.mcutils.events.player.MCPlayerEvent;
+import net.codersky.mcutils.regions.Region;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+
+import javax.annotation.Nonnull;
+
+public class RegionEnterEvent extends MCPlayerEvent {
+
+	private final Region region;
+	private final static HandlerList handlers = new HandlerList();
+
+	public RegionEnterEvent(@Nonnull Player player, @Nonnull Region region) {
+		super(player);
+		this.region = region;
+	}
+
+	@Nonnull
+	public final Region getRegion() {
+		return region;
+	}
+
+	@Nonnull
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	@Nonnull
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
